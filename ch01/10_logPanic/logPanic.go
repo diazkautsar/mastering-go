8c6d43ca799d2d02357d5e7653d2eae9270082bd
Diaz Kautsar <diaz@amg.co.id>
2025-01-06 16:08:20 +0700

    ch01 log files, log fatal, log panic, new error, errors

---
+++ ch01/10_logPanic/logPanic.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+)
+
+func main() {
+	syslog, err := syslog.New(syslog.LOG_ALERT|syslog.LOG_MAIL, "Some program!")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.SetOutput(syslog)
+	}
+
+	log.Panic(syslog)
+	fmt.Println("Will you see this?")
+}
